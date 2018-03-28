2bdabea1d7e483cc56548d2930a0babdb4108d26
ballum1 <benedictallum@gmail.com>
2018-03-28 17:22:05 +0100

    changed order to check for settings initially, and then dedupe OR train depending on whether settings exists

--- postgres/pgsql_tenders.py
+++ postgres/pgsql_tenders.py
@@ -1,12 +1,22 @@
 """
 script for deduping tender data.
 
-Works as follows
+Works as follows:
 
-1) Pulls data from the tenders table/view
-2) Looks for training data/settings, if none found then asks for training data and trains model
-3) Dedupes the data using that model/settings
-4) Uploads deduped table to database
+First checks to see if a settings file exists (which stores the deduping model).
+
+IF settings file exists:
+
+1) loads deduper from settings file.
+2) creates a table for storing dedupe results.
+3) pulls data to be deduped from database.
+2) dedupes data using the loaded settings.
+3) Adds the deduped data to table.
+
+IF settings file doesn't exist:
+
+1) pulls data from database to use to train a new deduping model
+2) uses user input
 
 Only outputs the clustered records (i.e. doesn't show records that didn't end up in a cluster)
 relies on the "id" field being the first field read
@@ -27,13 +37,36 @@ from unidecode import unidecode
 
 
 # settings and training files
+# if settings file does not exist then new model will be trained
 settings_file = 'tender_settings_new_structure'
 training_file = 'tender_training_new_structure.json'
 
-# select the country for deduping, and the "enddate" range
-search_source = "ocds.ocds_tenders_view"
-search_country = "United Kingdom"
-search_date_range = ['2017-01-01', '2017-01-10']
+# select where to get the data for deduping from, the country for deduping, and the "enddate" ranges
+select_source = "ocds.ocds_tenders_view"
+select_country = "United Kingdom"
+select_date_ranges = [['2017-01-01', '2017-01-10'],
+                      ['2017-01-11', '2017-01-21']]
+
+# specify fields to pull from the tenders table (make sure to pull all the fields you need for deduping later)
+input_fields = ["id",
+                "title",
+                "value",
+                "description",
+                "buyer",
+                "postcode",
+                "email"
+                ]
+
+# column names that you want in the results table (make sure to have the cluster_id field first)
+output_fields = ["cluster_id"] + input_fields + ["startdate", "enddate"]
+
+# name of results table
+results_table = "ocds_tenders_deduped_new"
+
+# settings for pulling data to train the deduper
+training_source = select_source
+training_country = select_country
+training_date_range = ['2017-01-01', '2017-01-10']
 
 # local database details (to use when testing)
 dbname = "postgres"
@@ -47,24 +80,6 @@ host_remote = os.environ.get("HOST_REMOTE")
 dbname_remote = os.environ.get("DBNAME_REMOTE")
 user_remote = os.environ.get("USER_REMOTE")
 password_remote = os.environ.get("PASSWORD_REMOTE")
-
-
-# specify fields to pull from the tenders table (make sure to pull all the fields you need for deduping later)
-input_fields = ["id",
-                "title",
-                "value",
-                "description",
-                "buyer",
-                "postcode",
-                "email"
-                ]
-
-# column names that you want in the results table
-# make sure to have the cluster_id field first
-output_fields = ["cluster_id"] + input_fields + ["startdate", "enddate"]
-
-# name of results table
-results_table = "ocds_tenders_deduped_NEW"
 
 def debugger_setup():
     optp = optparse.OptionParser()
@@ -179,7 +194,7 @@ def collect_labelled_data(data_d):
     with open(settings_file, 'w') as sf:
         deduper.writeSettings(sf)
 
-    return deduper
+    return
 
 def custom_dedupe(deduper, data_d):
     """dedupes the dictionary data_d using deduper. Returns the clustered dupes"""
@@ -210,10 +225,10 @@ def create_table(table_name, column_names):
     con.close()
     return
 
-def add_data_to_table(table_name, query, column_names, clustered_dupes):
+def add_data_to_table(table_name, query, column_names, clustered_dupes, clusters_index_start=0):
     """adds the deduped data to table"""
 
-    print ('connecting to db...')
+    # start connection
     con = psy.connect(host=host_remote, dbname=dbname_remote, user=user_remote, password=password_remote)
     c = con.cursor()
 
@@ -226,12 +241,11 @@ def add_data_to_table(table_name, query, column_names, clustered_dupes):
     full_data = []  # list to be inserted into the deduped table
 
     cluster_membership = collections.defaultdict(lambda: 'x')  # This variable doesn't seem to be used anywhere else?
-    for cluster_id, (cluster, score) in enumerate(clustered_dupes):  # cycle through the clustered dupes
+    for cluster_id, (cluster, score) in enumerate(clustered_dupes, clusters_index_start):  # cycle through the clustered dupes, start clusters_id where the last one left off
         for record_id in cluster:
             for row in data:  # data is a list, but isn't each row a dict? NO because data was redefined above
                 # so I think this assumes that the the first value in each row from the table is an id
                 # so if the row id matches the record_id from the clustered_dupes...
-                # this next section pulls the
                 if record_id == int(row[0]):
                     row = list(row)  # turn the tuple into a list
                     row.insert(0, cluster_id)  # put the cluster_id at the beginning of the list
@@ -239,127 +253,72 @@ def add_data_to_table(table_name, query, column_names, clustered_dupes):
                     full_data.append(row)  # add the new row to a new list called fulldata
 
     # Input the data into the dedupe table
-    # make new connection (not sure I need this?)
-    con3 = psy.connect(host=host_remote, dbname=dbname_remote, user=user_remote, password=password_remote)
-    c3 = con3.cursor()
+    # make new connection
+    con2 = psy.connect(host=host_remote, dbname=dbname_remote, user=user_remote, password=password_remote)
+    c2 = con2.cursor()
 
     num_cols = len(column_names)
     mog = "(" + ("%s," * (num_cols - 1)) + "%s)"
-    args_str = ','.join(c3.mogrify(mog, x) for x in full_data)  # mogrify is used to make query strings
+    args_str = ','.join(c2.mogrify(mog, x) for x in full_data)  # mogrify is used to make query strings
     values = "(" + ','.join(x for x in column_names) + ")"
-    c3.execute("INSERT INTO ocds.{} {} VALUES {}".format(table_name, values, args_str))
-    con3.commit()
-    con3.close()
+    c2.execute("INSERT INTO ocds.{} {} VALUES {}".format(table_name, values, args_str))
+    con2.commit()
+    con2.close()
 
     return
 
 
-#
-#
-#
-#
-#
-#
-# # BEGIN CONSTRUCTING THE TABLE TO STORE THE DEDUPED DATA
-# # connect to ocds again to get the data and to get the columns to make the deduped table
-# con2 = psy.connect(host=host_remote, dbname=dbname_remote, user=user_remote, password=password_remote)
-# c2 = con2.cursor()
-#
-# # use restricted number of columns for now, to avoid the JSON problem
-# # c2.execute("SELECT id, source, source_id, ocid, language, title FROM ocds.ocds_tenders_view where countryname = 'United Kingdom' limit 500")
-#
-# # use the SELECT_DATA for now, get the data which will ultimately be put into the _deduped table (along with the cluster ids)
-# input_fields += ["startdate", "enddate"]
-# DATA_SELECT2 = "select {} from ocds.ocds_tenders_view where countryname = '{}' and enddate between '{}' and '{}'".format(", ".join(input_fields), country, date_range_start, date_range_end)
-#
-# c2.execute(DATA_SELECT2)
-# data = c2.fetchall() # returns a list of tuples
-#
-# full_data = [] # list to be inserted into the deduped table
-#
-# cluster_membership = collections.defaultdict(lambda: 'x') # This variable doesn't seem to be used anywhere else?
-# for cluster_id, (cluster, score) in enumerate(clustered_dupes): # cycle through the clustered dupes
-#     for record_id in cluster:
-#         for row in data: # data is a list, but isn't each row a dict? NO because data was redefined above
-#             # so I think this assumes that the the first value in each row from the table is an id
-#             # so if the row id matches the record_id from the clustered_dupes...
-#             # this next section pulls the
-#             if record_id == int(row[0]):
-#                 row = list(row) # turn the tuple into a list
-#                 row.insert(0, cluster_id) # put the cluster_id at the beginning of the list
-#                 row = tuple(row) # make it a tuple again
-#                 full_data.append(row) # add the new row to a new list called fulldata
-#
-# # specify column names to have in the deduped table, for now use same ones as the input fields
-# column_names = input_fields
-#
-# # NOTE: having problemw wuth code below because the json format is interfering with the column name retrieval for some reason...
-#
-# # columns = "SELECT column_name FROM information_schema.columns WHERE table_name = 'ocds_tenders_view'"
-# # c2.execute(columns)
-# # column_names = c2.fetchall()
-# # column_names = [x[0] for x in column_names]
-# # # for now, only use the first seven columns
-# # column_names = column_names[:7]
-# column_names.insert(0, 'cluster_id') # add cluster_id to the column names
-#
-# # close con2
-# con2.close()
-#
-# # new connection to make the table for the deduped data
-# con3 = psy.connect(host=host_remote, dbname=dbname_remote, user=user_remote, password=password_remote)
-# c3 = con3.cursor()
-#
-# # create the dedupe table
-# c3.execute('DROP TABLE IF EXISTS ocds.ocds_tenders_deduped8') # get rid of the table (so we can make a new one)
-# field_string = ','.join('%s varchar(500000)' % name for name in column_names)
-# c3.execute('CREATE TABLE ocds.ocds_tenders_deduped8 (%s)' % field_string)
-# con3.commit()
-#
-# # Input the data into the dedupe table
-# num_cols = len(column_names)
-# mog = "(" + ("%s," * (num_cols - 1)) + "%s)"
-# args_str = ','.join(c3.mogrify(mog, x) for x in full_data) # mogrify is used to make query strings
-# values = "(" + ','.join(x for x in column_names) + ")"
-# c3.execute("INSERT INTO ocds.ocds_tenders_deduped8 %s VALUES %s" % (values, args_str))
-# con3.commit()
-# con3.close()
-#
-# print ('ran in', time.time() - start_time, 'seconds')
 
 if __name__ == '__main__':
 
     start_time = time.time()
     debugger_setup()
 
-    # construct the query for getting data to be deduped
-    select_query = construct_query(input_fields, search_source, search_date_range, search_country)
-
-    # get data
-    select_data = fetch_data(select_query)
-
-    # clean the data
-    select_dict = clean_data(select_data)
-
-    # get the deduper it it exists
+    # if settings file exists
     if os.path.exists(settings_file):
-        # load from settings file if that's possible
+        # load from settings file
         print ('reading from', settings_file)
         with open(settings_file) as sf:
             deduper = dedupe.StaticDedupe(sf)
 
-    # otherwise collect labelled data and train it
+        # construct results table
+        create_table(results_table, output_fields)
+        # start the cluster_id at 0
+        current_index = 0
+
+        # loop over different dates
+        for date_range in select_date_ranges:
+
+            # construct the query for getting data to be deduped
+            select_query = construct_query(input_fields, select_source, date_range, select_country)
+
+            # get data from the database using the query
+            select_data = fetch_data(select_query)
+
+            # clean the data
+            cleaned_data = clean_data(select_data)
+
+            # proceed to deduping:
+            clusters = custom_dedupe(deduper, cleaned_data)
+    
+            # add the results to the table
+            # make query to get the rest of the data we want in the table
+            output_query = construct_query(output_fields[1:], select_source, date_range, select_country)
+            add_data_to_table(results_table, output_query, output_fields, clusters, clusters_index_start=current_index)
+
+            # update the index for clusters_id (for the next set of clusters to be added to the results table)
+            current_index = len(clusters)
+
+    # if the settings file doesn't exist, this means we need to train a new deduping model
     else:
-        deduper = collect_labelled_data(select_dict)
+        # import data for training
+        select_query = construct_query(input_fields, training_source, training_date_range, training_country)
+        training_data = fetch_data(select_query)
+        cleaned_training_data = clean_data(training_data)
 
-    # now we have deduper, proceed to deduping:
-    clusters = custom_dedupe(deduper, select_dict)
+        # get labelled training examples and train a new model
+        collect_labelled_data(cleaned_training_data)
+        print("training complete. Run script again to begin deduping with the newly trained model")
 
-    # construct results table
-    create_table(results_table, output_fields)
-
-    # add the results to the table
-    output_query = construct_query(output_fields[1:], search_source, search_date_range, search_country)
-    add_data_to_table(results_table, output_query, output_fields, clusters)
 
     print ('ran in', time.time() - start_time, 'seconds')
